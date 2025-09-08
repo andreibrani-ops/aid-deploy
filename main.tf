85dbda95f67f99d59d8d275d510d38b7e48dad44
Andrei Branisteanu <AndreiCalin.Branisteanu1@stefanini.com>
2025-09-08 11:19:52 +0300

    created the network infrastructure + 2 VMs as session host to the pooled hp

--- main.tf
+++ main.tf
@@ -43,3 +43,127 @@ resource "azurerm_virtual_desktop_host_pool" "pooled" {
     Customer    = var.customer_name
   }
 }
+
+resource "azurerm_virtual_network" "avd" {
+  name                = "vnet-${local.resource_prefix}-avd"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.avd.location
+  resource_group_name = azurerm_resource_group.avd.name
+
+  tags = {
+    Environment = var.environment
+    Customer    = var.customer_name
+  }
+}
+
+resource "azurerm_subnet" "avd" {
+  name                 = "subnet-${local.resource_prefix}-avd"
+  resource_group_name  = azurerm_resource_group.avd.name
+  virtual_network_name = azurerm_virtual_network.avd.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_security_group" "avd" {
+  name                = "nsg-${local.resource_prefix}-avd"
+  location            = azurerm_resource_group.avd.location
+  resource_group_name = azurerm_resource_group.avd.name
+
+  security_rule {
+    name                       = "RDP"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    Environment = var.environment
+    Customer    = var.customer_name
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "avd" {
+  subnet_id                 = azurerm_subnet.avd.id
+  network_security_group_id = azurerm_network_security_group.avd.id
+}
+
+resource "azurerm_virtual_desktop_host_pool_registration_info" "pooled" {
+  hostpool_id     = azurerm_virtual_desktop_host_pool.pooled.id
+  expiration_date = timeadd(timestamp(), "48h")
+}
+
+resource "azurerm_network_interface" "session_host" {
+  count               = 2
+  name                = "nic-${local.resource_prefix}-sh-${count.index + 1}"
+  location            = azurerm_resource_group.avd.location
+  resource_group_name = azurerm_resource_group.avd.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.avd.id
+    private_ip_address_allocation = "Dynamic"
+  }
+
+  tags = {
+    Environment = var.environment
+    Customer    = var.customer_name
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "session_host" {
+  count               = 2
+  name                = "vm-${local.resource_prefix}-sh-${count.index + 1}"
+  resource_group_name = azurerm_resource_group.avd.name
+  location            = azurerm_resource_group.avd.location
+  size                = var.vm_size
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+
+  network_interface_ids = [
+    azurerm_network_interface.session_host[count.index].id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsDesktop"
+    offer     = "Windows-10"
+    sku       = "win10-21h2-avd"
+    version   = "latest"
+  }
+
+  tags = {
+    Environment = var.environment
+    Customer    = var.customer_name
+  }
+}
+
+resource "azurerm_virtual_machine_extension" "avd_agent" {
+  count                = 2
+  name                 = "avd-agent-${count.index + 1}"
+  virtual_machine_id   = azurerm_windows_virtual_machine.session_host[count.index].id
+  publisher            = "Microsoft.Powershell"
+  type                 = "DSC"
+  type_handler_version = "2.73"
+
+  settings = jsonencode({
+    modulesUrl            = "https://wvdportalstorageblob.blob.core.windows.net/galleryartifacts/Configuration_3-10-2021.zip"
+    configurationFunction = "Configuration.ps1\\AddSessionHost"
+    properties = {
+      hostPoolName          = azurerm_virtual_desktop_host_pool.pooled.name
+      registrationInfoToken = azurerm_virtual_desktop_host_pool_registration_info.pooled.token
+    }
+  })
+
+  tags = {
+    Environment = var.environment
+    Customer    = var.customer_name
+  }
+}
--- variables.tf
+++ variables.tf
@@ -15,13 +15,31 @@ variable "customer_name" {
 variable "location" {
   description = "Azure region for resources"
   type        = string
-  default     = "East US"
+  default     = "West Europe"
 }
 
 variable "resource_group_name" {
   description = "Name of the resource group"
   type        = string
   default     = ""
+}
+
+variable "vm_size" {
+  description = "Size of the virtual machines"
+  type        = string
+  default     = "Standard_D2s_v3"
+}
+
+variable "admin_username" {
+  description = "Administrator username for VMs"
+  type        = string
+  default     = "avdadmin"
+}
+
+variable "admin_password" {
+  description = "Administrator password for VMs"
+  type        = string
+  sensitive   = true
 }
 
 locals {
